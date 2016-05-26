c8a56b9784ab8472994d93276d810b2ad05c2035
kferna34 <kferna34@my.centennialcollege.ca>
2016-05-26 13:11:03 -0400

    Initial commit== created classes and unit test

---
+++ UnitTestForLesson03/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForLesson03
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
