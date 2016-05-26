1b9e6e4e5b8b5d81ddc2f00d0a54860df96e7628
kferna34 <kferna34@my.centennialcollege.ca>
2016-05-26 13:17:41 -0400

    program class code done

--- W3_L3/Program.cs
+++ W3_L3/Program.cs
@@ -6,10 +6,53 @@ using System.Threading.Tasks;
 
 namespace W3_L3
 {
-    class Program
+
+    /**
+
+    * This class is the "driver" class for our Program
+
+    *
+
+    * @class Program
+
+*/
+
+    public class Program
+
     {
-        static void Main(string[] args)
+
+        /**
+
+        * the main method for our driver class Program
+
+        *
+
+        * @method Main
+
+        * @param {sting[]} args
+
+*/
+
+        public static void Main(string[] args)
+
         {
+
+            // create a new instance of the Person class
+
+            Person person = new Person("Tom");
+
+            Console.WriteLine(person.Name);
+
+            // set a value in the Name property of the Person class
+
+            //person.Name = "Tom";
+
+            // get the Name value and print to console
+
+            Console.WriteLine(person.Name);
+
         }
+
     }
+
 }
